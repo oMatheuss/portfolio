42bc987c87f9fd7acb36cef31de6ca8371744bc3
Matheus <matheussmoura@outlook.com>
2023-05-25 14:27:43 -0300

    Fix typos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           className="h-12 w-12 rounded-md mb-4"
         />
         <h1 className="text-4xl text-black bold mb-2 tracking-wider font-extrabold dark:text-white">
-          Hi, I&apos;am Matheus Moura
+          Hi, I&apos;m Matheus Moura
         </h1>
         <h2 className="text-2xl text-red-600 tracking-wider mb-6">
           FullStack Developer
@@ -36,7 +36,7 @@ export default function Home() {
           , for now only available in Portuguese.
         </p>
         <p className="mb-4">
-          I&apos;am brazilian, and I live in Uberlândia - MG, Brazil.
+          I&apos;m brazilian, and I live in Uberlândia - MG, Brazil.
         </p>
         <p className="mb-4">
           You can contact me at{" "}
