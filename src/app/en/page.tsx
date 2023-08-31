bafbbff1880b85dca1b746c65f8d7f33cab87e34
Matheus <matheussmoura@outlook.com>
2023-08-31 10:07:14 -0300

    added linter, new page & minor adjustments

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -1,46 +1,54 @@
-import Anchor from "@/components/Anchor";
-import Image from "next/image";
+import Anchor from '@/components/Anchor';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <nav className="mb-8">
-        <hr className="border-y-4 border-red-600" />
+      <nav className='mb-8'>
+        <hr className='border-y-4 border-red-600' />
       </nav>
-      <main className="text-lg mx-2 sm:max-w-prose sm:mx-auto leading-relaxed antialiased">
+      <main className='text-lg mx-2 sm:max-w-prose sm:mx-auto leading-relaxed antialiased'>
         <Image
-          src="/android-chrome-192x192.png"
+          src='/android-chrome-192x192.png'
           width={192}
           height={192}
-          alt="Logo"
-          className="h-12 w-12 rounded-md mb-4"
+          alt='Logo'
+          className='h-12 w-12 rounded-md mb-4'
         />
-        <h1 className="text-4xl text-black bold mb-2 tracking-wider font-extrabold dark:text-white">
+        <h1 className='text-4xl text-black bold mb-2 tracking-wider font-extrabold dark:text-white'>
           Hi, I&apos;m Matheus Moura
         </h1>
-        <h2 className="text-2xl text-red-600 tracking-wider mb-6">
+        <h2 className='text-2xl text-red-600 tracking-wider mb-6'>
           FullStack Developer
         </h2>
-        <p className="mb-4">
+        <p className='mb-4'>
           I work mostly with building light and fast web applications.
         </p>
-        <p className="mb-4">
+        <p className='mb-4'>
           I believe that my applications should always be special.
         </p>
-        <p className="mb-4">
-          My commitment to what I develop extends even to my hobbies. And
-          because of my fascination with games, one of the games I created was{" "}
-          <Anchor href="https://www.typergame.app/" target="_blank">
-            TyperGame
-          </Anchor>
-          , for now only available in Portuguese.
+        <p className='mb-2'>
+          I have already developed a few applications, among them:
         </p>
-        <p className="mb-4">
+        <ul className='mb-4 list-disc list-inside space-y-2'>
+          <li>
+            <Anchor href='https://www.typergame.app/' target='_blank'>
+              TyperGame
+            </Anchor>
+            <span className='text-sm ml-2'>(only portuguese)</span>
+          </li>
+          <li>
+            <Anchor href='https://polyrhythm-chi.vercel.app/' target='_blank'>
+              Polyrhythm
+            </Anchor>
+          </li>
+        </ul>
+        <p className='mb-4'>
           I&apos;m brazilian, and I live in Uberlândia - MG, Brazil.
         </p>
-        <p className="mb-4">
-          You can contact me at{" "}
-          <Anchor href="mailto:matheussmoura@outlook.com">
+        <p className='mb-4'>
+          You can contact me at{' '}
+          <Anchor href='mailto:matheussmoura@outlook.com'>
             matheussmoura@outlook.com
           </Anchor>
           .
--- src/app/pt-br/page.tsx
+++ src/app/pt-br/page.tsx
@@ -1,45 +1,50 @@
-import Anchor from "@/components/Anchor";
-import Image from "next/image";
+import Anchor from '@/components/Anchor';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <nav className="mb-8">
-        <hr className="border-y-4 border-red-600" />
+      <nav className='mb-8'>
+        <hr className='border-y-4 border-red-600' />
       </nav>
-      <main className="text-lg mx-2 sm:max-w-prose sm:mx-auto leading-relaxed antialiased">
+      <main className='text-lg mx-2 sm:max-w-prose sm:mx-auto leading-relaxed antialiased'>
         <Image
-          src="/android-chrome-192x192.png"
+          src='/android-chrome-192x192.png'
           width={192}
           height={192}
-          alt="Logo"
-          className="h-12 w-12 rounded-md mb-4"
+          alt='Logo'
+          className='h-12 w-12 rounded-md mb-4'
         />
-        <h1 className="text-4xl text-black bold mb-2 tracking-wider font-extrabold dark:text-white">
+        <h1 className='text-4xl text-black bold mb-2 tracking-wider font-extrabold dark:text-white'>
           Oi, eu sou Matheus Moura
         </h1>
-        <h2 className="text-2xl text-red-600 tracking-wider mb-6">
+        <h2 className='text-2xl text-red-600 tracking-wider mb-6'>
           Desenvolvedor FullStack
         </h2>
-        <p className="mb-4">
+        <p className='mb-4'>
           Trabalho principalmente com aplicações web que devem ser rapidas e
           leves.
         </p>
-        <p className="mb-4">
+        <p className='mb-4'>
           Acredito que minhas aplicações devem sempre ser especiais.
         </p>
-        <p className="mb-4">
-          Meu comprometimento com o que desenvolvo se estende até mesmo aos meus
-          hobbies. E por meu fascínio por jogos, um dos que eu criei foi o{" "}
-          <Anchor href="https://www.typergame.app/" target="_blank">
-            TyperGame
-          </Anchor>
-          .
-        </p>
-        <p className="mb-4">Sou brasileiro, e moro em Uberlândia - MG.</p>
-        <p className="mb-4">
-          Você pode me contatar no meu email:{" "}
-          <Anchor href="mailto:matheussmoura@outlook.com">
+        <p className='mb-2'>Já desenvolvi diversas aplicações, dentre elas:</p>
+        <ul className='mb-4 list-disc list-inside space-y-2'>
+          <li>
+            <Anchor href='https://www.typergame.app/' target='_blank'>
+              TyperGame
+            </Anchor>
+          </li>
+          <li>
+            <Anchor href='https://polyrhythm-chi.vercel.app/' target='_blank'>
+              Polyrhythm
+            </Anchor>
+          </li>
+        </ul>
+        <p className='mb-4'>Sou brasileiro, e moro em Uberlândia - MG.</p>
+        <p className='mb-4'>
+          Você pode me contatar no meu email:{' '}
+          <Anchor href='mailto:matheussmoura@outlook.com'>
             matheussmoura@outlook.com
           </Anchor>
           .
