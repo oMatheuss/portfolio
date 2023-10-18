cb395ae1fa8dd23d5f64cebfef867ace5397b053
Matheus <matheussmoura@outlook.com>
2023-10-18 14:03:36 -0300

    update dependencies & middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,32 @@
-import createMiddleware from "next-intl/middleware";
- 
-export default createMiddleware({
-  locales: ['en', 'pt-br'],
-  defaultLocale: 'pt-br'
-});
- 
+import { match } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
+import { type NextRequest, NextResponse } from 'next/server';
+
+const locales = ['en', 'pt-br'];
+
+function getLocale(request: NextRequest) {
+  const headers = { ...Object.fromEntries(request.headers.entries()) };
+  let languages = new Negotiator({ headers }).languages();
+  let defaultLocale = 'en';
+
+  return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+
+  if (pathnameHasLocale) return;
+
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+
+  return NextResponse.redirect(request.nextUrl);
+}
+
 export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)']
+  matcher: ['/((?!_next).*)'],
 };
