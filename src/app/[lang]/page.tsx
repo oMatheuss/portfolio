f55b734f2262330f6599d70c360df2ae5d784546
matheus <matheussmoura@outlook.com>
2025-08-16 15:43:50 -0300

    improved styles and reduced text

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,23 +1,25 @@
 import Anchor from '@/components/anchor';
 import { type Langs, getDictionary } from './dictionaries';
-import { ArrowIcon } from '@/components/arrow-icon';
-import { GameOfLife } from '@/components/game-of-life';
+import {
+  ArrowRightIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+} from '@radix-ui/react-icons';
 
 export default async function Home({ params }: { params: { lang: Langs } }) {
   const dict = await getDictionary(params.lang);
 
   return (
     <>
-      <header className='px-4 py-2'>
+      <header className='py-2 xl:mx-auto xl:max-w-screen-xl'>
         <div className='flex h-10 items-center border-b-2 border-base-300'>
           <h1 className='inline px-2 align-middle text-xl font-extrabold'>
-            Matheus
+            Matheus Moura
           </h1>
         </div>
       </header>
       <main className='mx-4 leading-relaxed antialiased xl:mx-auto xl:max-w-screen-xl'>
-        <section className='intro section relative'>
-          <GameOfLife className='absolute z-[1] h-full w-full' />
+        <section className='relative mb-8'>
           <div className='z-[2] rounded bg-base-200/80 p-4 shadow dark:bg-base-200'>
             <h1 className='bold mb-2 text-6xl font-extrabold tracking-wider'>
               {dict.hi}
@@ -26,16 +28,40 @@ export default async function Home({ params }: { params: { lang: Langs } }) {
               {dict.role}
             </h2>
             <p className='mb-4 text-lg'>{dict.slogan}</p>
+            <div className='flex gap-2'>
+              <a
+                className='group block size-10 rounded bg-neutral p-1'
+                href='//www.linkedin.com/in/matheus-moura-a142b4177'
+                aria-label='Linkedin Profile'
+                target='_blank'
+              >
+                <LinkedInLogoIcon
+                  aria-hidden
+                  className='size-8 text-neutral-content group-hover:text-primary-content'
+                />
+              </a>
+              <a
+                className='group block size-10 rounded bg-neutral p-1'
+                href='//github.com/oMatheuss'
+                aria-label='GitHub Profile'
+                target='_blank'
+              >
+                <GitHubLogoIcon
+                  aria-hidden
+                  className='size-8 text-neutral-content group-hover:text-primary-content'
+                />
+              </a>
+            </div>
           </div>
         </section>
 
-        <section>
+        <section className='relative mb-8'>
           <h3 className='mb-2 text-3xl font-semibold'>
             {dict.experiencesTitle}
           </h3>
           <ol className='mb-4 space-y-2'>
             {dict.experiences.map((x, i) => (
-              <li key={i} className='rounded bg-base-200 p-4 shadow'>
+              <li key={i} className='rounded bg-base-200 px-4 py-2 shadow'>
                 <h4 className='font-medium'>
                   {x.company} - {x.role}
                 </h4>
@@ -48,11 +74,11 @@ export default async function Home({ params }: { params: { lang: Langs } }) {
           </ol>
         </section>
 
-        <section>
+        <section className='relative mb-8'>
           <h3 className='mb-2 text-3xl font-semibold'>{dict.educationTitle}</h3>
           <ol className='mb-4 space-y-2'>
             {dict.education.map((x, i) => (
-              <li key={i} className='rounded bg-base-200 p-4 shadow'>
+              <li key={i} className='rounded bg-base-200 px-4 py-2 shadow'>
                 <h4 className='font-medium'>{x.location}</h4>
                 <p className='text-sm'>
                   {x.start} - {x.end}
@@ -63,13 +89,13 @@ export default async function Home({ params }: { params: { lang: Langs } }) {
           </ol>
         </section>
 
-        <section>
+        <section className='relative mb-8'>
           <h3 className='mb-2 text-3xl font-semibold'>{dict.developed}</h3>
           <ul className='mb-4 grid grid-cols-1 gap-8 text-center sm:grid-cols-2 lg:grid-cols-3'>
             {dict.projects.map((repo, i) => (
               <li
                 key={i}
-                className='flex flex-col rounded bg-base-200 p-4 shadow'
+                className='flex flex-col rounded bg-base-200 px-4 py-2 shadow'
               >
                 <h4 className='mb-2 text-2xl font-bold capitalize leading-tight'>
                   {repo.title}
@@ -85,7 +111,7 @@ export default async function Home({ params }: { params: { lang: Langs } }) {
                   target='_blank'
                 >
                   Ver repositório
-                  <ArrowIcon className='-mr-1 ml-2 h-5 w-5 transition-transform group-hover:translate-x-1' />
+                  <ArrowRightIcon className='-mr-1 ml-2 h-5 w-5 transition-transform group-hover:translate-x-1' />
                 </a>
               </li>
             ))}
